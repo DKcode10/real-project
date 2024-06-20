a827e20fc936dd06a8c47701678ffc0d675dd460
DKcode10 <www.empiredk@gmail.com>
2024-06-20 21:54:18 +0530

    again

--- megablog/src/App.jsx
+++ megablog/src/App.jsx
@@ -1,13 +1,38 @@
-import React from 'react'
+import React, { useState , useEffect } from 'react'
+import  {useDispatch} from 'react-redux'
 import './App.css'
+import authService from './appwrite/auth'
+import {login,logout} from './store/authSlice'
+import { Footer, Header } from './components'
+import { Outlet } from 'react-router-dom'
 
 const App = () => {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  return (
-    <>
-     <h1>Home Sweet Home Please welcome to over house</h1>
-    </>
-  )
+
+  const[loading,setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+
+  useEffect (()=>{
+authService.getCurrentUser()
+.then((userDate)=>{
+  if (userDate) {
+    dispatch(login({userDate}))
+  }else{
+    dispatch(logout())
+  }
+})
+.finally(()=>setLoading(false))
+  },[])
+
+return !loading ? (
+  <div className='min-hhscreen flex flex-wrap content-between bg-gray-400 '> 
+  <div className='w-full block'>
+    <Header/>
+     TODO {/* <Outlet/> */}
+    <Footer/>
+    </div> 
+  </div>
+) : null
 }
 
 export default App
