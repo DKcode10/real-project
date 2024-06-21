75c684f4e4a6061573deb5c99c421881bc96eddb
DKcode10 <www.empiredk@gmail.com>
2024-06-21 14:05:49 +0530

    jf

--- megablog/src/components/Header/Header.jsx
+++ megablog/src/components/Header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
   const authStatus = useSelector((state) => state.auth.status);
   const navigate = useNavigate();
 
-  const naItems = [
+  const navItem = [
     {
       name: "Home",
       slug: "/",
@@ -32,7 +32,38 @@ const Header = () => {
       active: authStatus,
     },
   ];
-  return <div></div>;
+  return (
+    <header className="py-3 shadow bg-gray-500">
+      <container>
+        <nav className="flex">
+          <div className="mr-4 ">
+            <Link to="/">
+              <Logo width="70px" />
+            </Link>
+          </div>
+          <ul className="flex ml-auto">
+            {navItem.map((item) =>
+              item.active ? (
+                <li key={item.name}>
+                  <button
+                    onClick={() => navigate(item.slug)}
+                    className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
+                  >
+                    {item.name}
+                  </button>
+                </li>
+              ) : null
+            )}
+            {authStatus && (
+              <li>
+                <LogoutBtn />
+              </li>
+            )}
+          </ul>
+        </nav>
+      </container>
+    </header>
+  );
 };
 
 export default Header;
